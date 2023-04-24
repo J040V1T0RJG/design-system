adacb01ea37adb78e088709b2d221614d1c91755
João Vitor <joaovitor.jovg73@gmail.com>
2023-04-24 13:23:18 -0300

    :sparkles: feat: design system in react

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx", 
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+};
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,30 @@
-import {} from '@ignite-ui/tokens'
+import { Component, ComponentProps } from 'react'
+import { styled } from './styles'
 
-export function App() {
-  return <h1>Hello Word</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button> & Component
