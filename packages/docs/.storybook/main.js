b918fd50a12f1e6278c8ac311b789290ac99ab43
João Vitor <joaovitor.jovg73@gmail.com>
2023-04-25 14:37:18 -0300

    :sparkles: feat: design system components

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -3,7 +3,12 @@ module.exports = {
     "../src/pages/**/*.stories.mdx", 
     "../src/stories/**/*.stories.tsx"
   ],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "addons": [
+    "@storybook/addon-links", 
+    "@storybook/addon-essentials", 
+    "@storybook/addon-interactions", 
+    '@storybook/addon-a11y'
+  ],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
