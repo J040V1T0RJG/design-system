0755f57e503528c26310f3c7ea80aaec827a6106
João Vitor <joaovitor.jovg73@gmail.com>
2023-05-01 10:58:30 -0300

    :bug: fix: add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,5 +1,10 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, Text, TextInput, TextInputProps } from '@j040v1t0r-ignite-ui/react'
+import {
+  Box,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@j040v1t0r-ignite-ui/react'
 
 export default {
   title: 'Form/Text Input',
@@ -35,5 +40,6 @@ export const WithDisabled: StoryObj<TextInputProps> = {
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username',
   },
 }
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,13 @@
+# @j040v1t0r-ignite-ui/react
+
+## 2.0.1
+
+### Patch Changes
+
+- add some fixes to future projects
+
+## 2.0.0
+
+### Major Changes
+
+- add display name to all components
